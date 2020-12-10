e69fa05b573d4f993f67940e5c47818570a69add
ryooochin776 <73107957+ryooochin776@users.noreply.github.com>
2020-12-10 16:52:19 +0900

    修正4

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action :move_to_index, only: :index
   before_action :set_product, only: [:index, :create]
+  before_action :move_to_index, only: :index
   before_action :move_to_root_path, only: :index
 
   def index
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -4,13 +4,13 @@ class PurchaseAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 0 }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
     validates :token
     validates :user_id
-    validates :product_id, numericality: { other_than: 0 }
+    validates :product_id
   end
 
   def save
